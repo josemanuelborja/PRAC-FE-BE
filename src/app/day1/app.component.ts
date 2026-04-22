ba2fcc86f8e5a7ac4c0e2458c8fd59d7d5fce136
josemanuelborja <jmborja11296@liceo.edu.ph>
2026-04-23 00:01:32 +0800

    feature: DAY 1 COMPLETE

--- src/app/day1/app.component.ts
+++ src/app/day1/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule, NgIf],
   templateUrl: './app.component.html'
 })
 
@@ -14,7 +17,17 @@ export class AppComponent {
   coins: number = 0;
 
   addCoins() {
-    this.coins = this.coins + 5; // or this,coins += 5;
+    this.coins = this.coins + 5; // or this.coins += 5;
+  }
+
+  resetCoins() {
+    this.coins = 0;
+  }
+
+  decreaseCoins() {
+    if (this.coins >= 5) {
+      this.coins = this.coins - 5; // or this.coins -= 5;
+    }
   }
 
 }
