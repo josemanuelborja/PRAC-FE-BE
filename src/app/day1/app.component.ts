4e8f77278a313b0232c54cf793fe56f46d03cc12
josemanuelborja <jmborja11296@liceo.edu.ph>
2026-04-25 00:39:26 +0800

    feat: add CommonModule, FormsModule, and NgIf to AppComponent

--- src/app/day1/app.component.ts
+++ src/app/day1/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule, NgIf],
   templateUrl: './app.component.html'
 })
 
